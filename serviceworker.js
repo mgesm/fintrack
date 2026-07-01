f67e86e4cebd88c177e6a76875dededa213713f5
mgesm <soulbloody321@gmail.com>
2026-07-01 15:02:01 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME='fintrack-cache-v30';
+var CACHE_NAME='fintrack-cache-v31';
 var PRECACHE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',function(e){
