19f8a239662a921a2d787752c6bf0af3c019b042
mgesm <soulbloody321@gmail.com>
2026-09-01 14:18:27 +0200

    Actualizar caché de logos transparentes

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME='fintrack-cache-v92';
+var CACHE_NAME='fintrack-cache-v93';
 var CACHE_PREFIX='fintrack-cache-';
 var PRECACHE=['./','./index.html','./manifest.json','./supabase-js.min.js','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js','./vendor/exceljs.min.js'];
 
