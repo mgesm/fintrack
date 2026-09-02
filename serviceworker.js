4b5407e14200cf31c8d37d028fbac74d4d7552ea
mgesm <soulbloody321@gmail.com>
2026-09-02 16:19:44 +0200

    chore: refresh cache for JSON import

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME='fintrack-cache-v120';
+var CACHE_NAME='fintrack-cache-v121';
 var CACHE_PREFIX='fintrack-cache-';
 var PRECACHE=['./','./index.html','./manifest.json','./supabase-js.min.js','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js','./vendor/exceljs.min.js'];
 
